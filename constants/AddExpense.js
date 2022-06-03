3a7be15433e219cd0d1eada1b97171455eb8a164
SwapnilChatakwar17 <praful.gupta.21@gmail.com>
2022-06-03 16:37:36 +0530

    tt

---
+++ constants/AddExpense.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View , Button, Text } from 'react-native'
+
+const AddExpense = () => {
+    return (
+        <View style={{flex : 1,justifyContent: 'center', alignContent: 'center'}}>
+            <Text>Home View</Text>
+        </View>
+    )
+}
+
+export default AddExpense
---
+++ constants/AddModal.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddModal = () => {
+  return (
+    <View>
+        <Modal>
+            <Text>AddModal</Text>
+        </Modal>
+    </View>
+  )
+}
+
+export default AddModal
+
+const styles = StyleSheet.create({})
